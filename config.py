86350fe5c912fd897ff2e77fb8504a626d3396bb
Daktari <dindijjames@gmail.com>
2017-12-09 12:07:55 +0000

    Set up and do some basics

---
+++ config.py
@@ -0,0 +1,26 @@
+# config.py
+
+class Config(object):
+    """
+    Common configurations
+    """
+    # Put here all configurations that are common across all environments
+    
+class DevelopmentConfig(object):
+    """
+    Development configurations
+    """
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+    
+class ProductionConfig(object):
+    """
+    Production configurations
+    """
+    
+    DEBUG = False
+    
+app_config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}
