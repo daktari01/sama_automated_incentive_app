8fe7b2c551715de584e943866860749462a6eab4
Daktari <dindijjames@gmail.com>
2017-12-11 11:40:35 +0000

    [Bug fix]: Correct MySQL error

--- app/models.py
+++ app/models.py
@@ -19,7 +19,7 @@ class Employee(UserMixin, db.Model):
     username = db.Column(db.String(60), index=True, unique=True)
     emp_name = db.Column(db.String(100), index=True)
     password_hash = db.Column(db.String(128))
-    project_id = db.Column(db.Integer, db.ForeignKey('projects.id'))
+    subproject_id = db.Column(db.Integer, db.ForeignKey('projects.id'))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     is_admin = db.Column(db.Boolean, default=False)
     attendances = db.relationship('Attendance', backref='employee', 
@@ -56,72 +56,6 @@ class Employee(UserMixin, db.Model):
 def load_user(user_id):
     return Employee.query.get(int(user_id))
 
-class Incentive(db.Model):
-    """
-    Create an Incentive table
-    """
-
-    __tablename__ = 'incentives'
-
-    id = db.Column(db.Integer, primary_key=True)
-    inc_employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
-    subproject_id = db.Column(db.Integer, db.ForeignKey('subprojects.id'))
-    inc_attendances_id = db.Column(db.Integer, db.ForeignKey('attendances.id'))
-    production = db.Column(db.Integer)
-    av_qa_score = db.Column(db.Integer)
-    total_points = db.Column(db.Integer)
-    amount = db.Column(db.Float)
-
-
-class Project(db.Model):
-    """
-    Create a Project table
-    """
-
-    __tablename__ = 'projects'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(60), unique=True)
-    pro_employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
-    description = db.Column(db.String(200))
-    employees = db.relationship('Employee', backref='project',
-                                 lazy='dynamic')
-    subprojects = db.relationship('Subproject', backref='project', 
-                                    lazy='dynamic')
-
-    def __repr__(self):
-        return '<Project: {}>'.format(self.name)
-
-class Subproject(db.Model):
-    """
-    Create a Subproject class
-    """
-
-    __tablename__ = 'subprojects'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(60), unique=True)
-    description = db.Column(db.String(200))
-    project_id = db.Column(db.Integer, db.ForeignKey('projects.id'))
-    employees = db.relationship('Employee', backref='subproject', lazy='dynamic')
-
-    def __repr__(self):
-        return '<Subproject: {}>'.format(self.name)
-
-class Attendance(db.Model):
-    """
-    Create an Attendance class
-    """
-
-    __tablename__ = 'attendances'
-
-    id = db.Column(db.Integer, primary_key=True)
-    att_employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
-    leave_days = db.Column(db.Integer)
-    days_present = db.Column(db.Integer)
-    percentage_attendance = db.Column(db.Integer)
-
-
 class Role(db.Model):
     """
     Create a Role table
@@ -136,4 +70,64 @@ class Role(db.Model):
 
     def __repr__(self):
         return '<Role: {}>'.format(self.name)
+        
+class Project(db.Model):
+    """
+    Create a Project table
+    """
+
+    __tablename__ = 'projects'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), unique=True)
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
+    description = db.Column(db.String(200))
+    subprojects = db.relationship('Subproject', backref='project', 
+                                    lazy='dynamic')
+                                    
+class Subproject(db.Model):
+    """
+    Create a Subproject class
+    """
+
+    __tablename__ = 'subprojects'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), unique=True)
+    description = db.Column(db.String(200))
+    project_id = db.Column(db.Integer, db.ForeignKey('projects.id'))
+    employees = db.relationship('Employee', backref='subproject', lazy='dynamic')
+    incentives = db.relationship('Incentive', backref='subproject', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Subproject: {}>'.format(self.name)
+        
+class Attendance(db.Model):
+    """
+    Create an Attendance class
+    """
+
+    __tablename__ = 'attendances'
+
+    id = db.Column(db.Integer, primary_key=True)
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
+    leave_days = db.Column(db.Integer)
+    days_present = db.Column(db.Integer)
+    percentage_attendance = db.Column(db.Integer)
+    incentives = db.relationship('Incentive', backref='attendance', lazy='dynamic')
     
+class Incentive(db.Model):
+    """
+    Create an Incentive table
+    """
+
+    __tablename__ = 'incentives'
+
+    id = db.Column(db.Integer, primary_key=True)
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
+    subproject_id = db.Column(db.Integer, db.ForeignKey('subprojects.id'))
+    attendances_id = db.Column(db.Integer, db.ForeignKey('attendances.id'))
+    production = db.Column(db.Integer)
+    av_qa_score = db.Column(db.Integer)
+    total_points = db.Column(db.Integer)
+    amount = db.Column(db.Float)
